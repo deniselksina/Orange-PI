c864eb767e129b2017b84832b87c08df37980a3f
deniselksina <elksina40@gmail.com>
2025-02-18 09:56:58 +0300

    Create ethernet.h

---
+++ build/boyjack-os/kernel/kernel_sources/include/ethernet.h
@@ -0,0 +1,27 @@
+#ifndef ETHERNET_H
+#define ETHERNET_H
+
+#include <stdint.h>
+
+#define ETH_FRAME_SIZE      1518
+#define ETH_HEADER_SIZE     14
+
+// Структура для представления Ethernet интерфейса
+typedef struct {
+    uint8_t mac_address[6];  // MAC-адрес устройства
+    uint32_t ip_address;     // IP-адрес устройства
+    uint32_t netmask;        // Маска подсети
+    uint32_t gateway;        // Шлюз
+    uint32_t mtu;            // Максимальный размер передачи
+} ethernet_interface_t;
+
+// Инициализация Ethernet интерфейса
+void ethernet_init(ethernet_interface_t *interface);
+
+// Отправка данных через Ethernet
+int ethernet_send(ethernet_interface_t *interface, uint8_t *data, uint32_t size);
+
+// Прием данных с Ethernet интерфейса
+int ethernet_receive(ethernet_interface_t *interface, uint8_t *buffer, uint32_t size);
+
+#endif // ETHERNET_H
