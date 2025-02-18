efe7d2ccc92664c1cf4813cb81c9214be3be5d9f
deniselksina <elksina40@gmail.com>
2025-02-18 08:07:02 +0300

    Create user_settings.sh

---
+++ build/boyjack-os/configs/user_settings.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Персональные настройки пользователя
+USER_THEME="dark"
+USER_FONT="Roboto"
+USER_LANGUAGE="en_US"
+
+# Экранные настройки
+DISPLAY_RESOLUTION="1920x1080"
+DISPLAY_REFRESH_RATE="60Hz"
+
+# Поведение окна и приложений
+WINDOW_BEHAVIOR="maximize"
+APP_STARTUP="true"
