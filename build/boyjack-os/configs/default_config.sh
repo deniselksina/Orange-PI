c2c32885683c427bcee55c50776cfc43d4bc2499
deniselksina <elksina40@gmail.com>
2025-02-18 08:06:27 +0300

    Create default_config.sh

---
+++ build/boyjack-os/configs/default_config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Настройки сети
+NETWORK_INTERFACE="eth0"
+DNS_SERVER="8.8.8.8"
+
+# Базовые настройки безопасности
+SECURITY_LEVEL="high"
+FIREWALL_ENABLED="true"
+
+# Параметры времени
+TIMEZONE="Europe/Moscow"
+NTP_SERVER="pool.ntp.org"
+
+# Логирование
+LOG_LEVEL="info"
