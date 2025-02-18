13f56b51b59347dd8cb92e27555f8e3c71e3df14
deniselksina <elksina40@gmail.com>
2025-02-18 08:09:07 +0300

    Create app3_config.sh

---
+++ build/boyjack-os/configs/custom_configs/app3_config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Настройки для браузера
+BROWSER_CACHE_SIZE="512MB"
+BROWSER_USER_AGENT="Mozilla/5.0"
+BROWSER_PROXY="false"
+BROWSER_DOWNLOAD_DIR="/home/user/Downloads"
+
+# Настройки безопасности
+ENABLE_TRACKING_PROTECTION="true"
+ENABLE_SSL_CHECK="true"
